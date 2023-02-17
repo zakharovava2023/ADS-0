943d583ebb26c658fec23ff11794dee87082b313
zakharovava2023 <125122557+zakharovava2023@users.noreply.github.com>
2023-02-17 17:25:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if (a%b==0)
+    if (a%b == 0)
         return b;
-    if (b%a==0)
+    if (b%a == 0)
         return a;
-    if (a>b)
+    if (a > b)
         return gcd(a%b, b);
     return gcd(a, b%a);
 }
